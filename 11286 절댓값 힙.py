e18598a4210a401d4276dc72900dba994a25c071
dmschd0678@naver.com <63108606+dmschd0678@users.noreply.github.com>
2021-11-03 23:26:52 +0900

    Algorithm

---
+++ 11286 절댓값 힙.py
@@ -0,0 +1,17 @@
+import sys
+import heapq
+
+n = int(sys.stdin.readline())
+
+hq = []
+
+for _ in range(n):
+    value = int(sys.stdin.readline())
+
+    if value == 0:
+        try:
+            print(heapq.heappop(hq)[1])
+        except:
+            print(0)
+    else:
+        heapq.heappush(hq,(abs(value), value))
